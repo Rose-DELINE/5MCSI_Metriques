69e292cc41f696692c72ba041c205af0188c4712
Rose-DELINE <genevievedeline@gmail.com>
2025-12-02 10:51:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+from urllib.request import urlopen
+import json
+from flask import jsonify
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
@@ -14,6 +17,17 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
   
 if __name__ == "__main__":
   app.run(debug=True)
