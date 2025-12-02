9d3ece7ed624047364ac112590a33c99a37284ee
Rose-DELINE <genevievedeline@gmail.com>
2025-12-02 11:58:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,6 +32,7 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+  
   @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
