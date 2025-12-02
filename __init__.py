f7e4594fff41cf5bad87c541221463ed5c95b523
Rose-DELINE <genevievedeline@gmail.com>
2025-12-02 11:37:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
-# ===== IMPORTS =====
-from flask import Flask, render_template, jsonify
 from urllib.request import urlopen
 import json
+from flask import jsonify
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
 from datetime import datetime
-import sqlite3  # si tu en as besoin plus tard
+from urllib.request import urlopen
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
 
-# ===== INITIALISATION DE L'APPLICATION =====
-app = Flask(__name__)
-
-# ===== ROUTES =====
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
@@ -16,8 +17,6 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
-
-# ===== EXERCICE 3 : ROUTE /tawarano/ =====
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -29,7 +28,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  from flask import render_template
 
-# ===== BLOC PRINCIPAL =====
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
